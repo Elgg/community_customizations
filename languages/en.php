78dd8c7ee0e547a1e24240c59dc7390290d066a3
Cash Costello <cash.costello@gmail.com>
2011-10-02 08:40:46 -0400

    updated pages welcome message to point to our documentation and tech support group

--- languages/en.php
+++ languages/en.php
@@ -11,6 +11,12 @@ $english = array(
 	'customizations:widget:pm:desc' => 'Enables users to send private messages to you from your profile',
 	'customizations:pm:logged_out' => 'You must be logged in to send a private message.',
 	'customizations:pm:subject' => 'Message from %s',
+
+	'pages:welcomemessage' => 'Welcome to the pages tool of %s. Use this tool for creating documentation related to Elgg.
+		If you are looking for help, please post in the
+		<a href="http://community.elgg.org/pg/groups/179063/elgg-technical-support/">technical support group</a>.
+		If you are looking for the main Elgg documentation page, go to <a href="http://docs.elgg.org/">our wiki</a>.',
+
 );
 
 add_translation("en", $english);
