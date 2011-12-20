d565fc8bf3b76304708f0c50affb494610181ab9
Cash Costello <cash.costello@gmail.com>
2011-12-20 18:52:17 -0500

    started updating for Elgg 1.8

--- start.php
+++ start.php
@@ -4,7 +4,7 @@
  *
  */
 
-register_elgg_event_handler('init', 'system', 'customizations_init');
+elgg_register_event_handler('init', 'system', 'customizations_init');
 
 function customizations_init() {
 	global $CONFIG;
@@ -16,22 +16,16 @@ function customizations_init() {
 	// turn off site notifications for performance reasons
 	unregister_notification_handler('site');
 
-	register_elgg_event_handler('delete', 'user', 'customizations_purge_messages');
+	elgg_register_event_handler('delete', 'user', 'customizations_purge_messages');
 
 	// convert messageboard to private message interface
-	add_widget_type('messageboard', elgg_echo("customizations:widget:pm"), elgg_echo("customizations:widget:pm:desc"), "profile");
-	register_plugin_hook('forward', 'system', 'customizations_pm_forward');
+	elgg_register_widget_type('messageboard', elgg_echo("customizations:widget:pm"), elgg_echo("customizations:widget:pm:desc"), "profile");
+	elgg_register_plugin_hook_handler('forward', 'system', 'customizations_pm_forward');
 
 	// limit access to the add links
-	register_elgg_event_handler('pagesetup', 'system', 'customizations_remove_add_links');
-	register_plugin_hook('action', 'bookmarks/add', 'customizations_stop_add');
-	register_plugin_hook('action', 'pages/edit', 'customizations_stop_add');
-	//register_plugin_hook('action', 'pages/editwelcome', 'customizations_stop_add');
-
-	// override the pages page handler to pull out link to add welcome message
-	register_page_handler('pages','customizations_pages_page_handler');
-	//unregister_action("pages/editwelcome");
-	unset($CONFIG->actions["pages/editwelcome"]);
+	elgg_register_event_handler('pagesetup', 'system', 'customizations_remove_add_links');
+	elgg_register_plugin_hook_handler('action', 'bookmarks/add', 'customizations_stop_add');
+	elgg_register_plugin_hook_handler('action', 'pages/edit', 'customizations_stop_add');
 
 	// do not want the pages link in hover menu
 	elgg_unextend_view('profile/menu/links', 'pages/menu');
@@ -41,7 +35,7 @@ function customizations_init() {
 	elgg_extend_view('groups/left_column', 'bookmarks/groupprofile_bookmarks');
 
 	$action_path = "{$CONFIG->pluginspath}community_customizations/actions";
-	register_action('comment/edit', FALSE, "$action_path/edit_comment.php", TRUE);
+	elgg_register_action('comment/edit', "$action_path/edit_comment.php", 'admin');
 }
 
 /**
@@ -58,7 +52,7 @@ function customizations_pm_forward() {
  * @return bool
  */
 function customizations_is_new_user() {
-	$user = get_loggedin_user();
+	$user = elgg_get_logged_in_user_entity();
 
 	// 2 days
 	$cutoff = time() - 2 * 24 * 60 * 60;
@@ -73,7 +67,7 @@ function customizations_is_new_user() {
  * Remove some add links for new users
  */
 function customizations_remove_add_links() {
-	if (isloggedin()) {
+	if (elgg_is_logged_in()) {
 		if (customizations_is_new_user()) {
 			// remove bookmark links
 			remove_submenu_item(elgg_echo('bookmarks:add'));
@@ -94,97 +88,8 @@ function customizations_remove_add_links() {
 function customizations_stop_add() {
 	if (customizations_is_new_user()) {
 		// spammer tried to directly hit the action
-		ban_user(get_loggedin_userid(), 'tried to post content before allowed');
+		ban_user(elgg_get_logged_in_user_guid(), 'tried to post content before allowed');
 		return false;
-	}
-}
-
-/**
- * Poor design requires us to override the pages plugin's page handler just to
- * remove the "add welcome message" link on the owner page. People are using it
- * for spam so we're just going to remove it
- */
-function customizations_pages_page_handler($page) {
-	global $CONFIG;
-
-	if (isset($page[0])) {
-		// See what context we're using
-		switch ($page[0]) {
-			case "new":
-				include($CONFIG->pluginspath . "pages/new.php");
-				break;
-			case "welcome":
-				if (isset($page[1])) {
-					set_input('username', $page[1]);
-				}
-				include($CONFIG->pluginspath . "pages/welcome.php");
-				break;
-			case "all":
-				include($CONFIG->pluginspath . "pages/world.php");
-				break;
-			case "owned":
-				// Owned by a user
-				if (isset($page[1])) {
-					set_input('username', $page[1]);
-				}
-
-				include($CONFIG->pluginspath . "community_customizations/pages/pages_owner_page.php");
-				break;
-			case "edit":
-				if (isset($page[1])) {
-					$guid = (int) $page[1];
-					set_input('page_guid', $guid);
-
-					$entity = get_entity($guid);
-					add_submenu_item(elgg_echo('pages:label:view'), $CONFIG->url . "pg/pages/view/$guid", 'pageslinks');
-					// add_submenu_item(elgg_echo('pages:user'), $CONFIG->wwwroot . "pg/pages/owned/" . $_SESSION['user']->username, 'pageslinksgeneral');
-					if (($entity) && ($entity->canEdit())) {
-						add_submenu_item(elgg_echo('pages:label:edit'), $CONFIG->url . "pg/pages/edit/$guid", 'pagesactions');
-					}
-					add_submenu_item(elgg_echo('pages:label:history'), $CONFIG->url . "pg/pages/history/$guid", 'pageslinks');
-				}
-
-				include($CONFIG->pluginspath . "pages/edit.php");
-				break;
-			case "view":
-
-				if (isset($page[1])) {
-					$guid = (int) $page[1];
-					set_input('page_guid', $guid);
-
-					elgg_extend_view('metatags', 'pages/metatags');
-
-					$entity = get_entity($guid);
-					//add_submenu_item(elgg_echo('pages:label:view'), $CONFIG->url . "pg/pages/view/$guid", 'pageslinks');
-					if (($entity) && ($entity->canEdit())) {
-						add_submenu_item(elgg_echo('pages:label:edit'), $CONFIG->url . "pg/pages/edit/$guid", 'pagesactions');
-					}
-					add_submenu_item(elgg_echo('pages:label:history'), $CONFIG->url . "pg/pages/history/$guid", 'pageslinks');
-				}
-
-				include($CONFIG->pluginspath . "pages/view.php");
-				break;
-			case "history":
-				if (isset($page[1])) {
-					$guid = (int) $page[1];
-					set_input('page_guid', $guid);
-
-					elgg_extend_view('metatags', 'pages/metatags');
-
-					$entity = get_entity($guid);
-					add_submenu_item(elgg_echo('pages:label:view'), $CONFIG->url . "pg/pages/view/$guid", 'pageslinks');
-					if (($entity) && ($entity->canEdit())) {
-						add_submenu_item(elgg_echo('pages:label:edit'), $CONFIG->url . "pg/pages/edit/$guid", 'pagesactions');
-					}
-					add_submenu_item(elgg_echo('pages:label:history'), $CONFIG->url . "pg/pages/history/$guid", 'pageslinks');
-				}
-
-				include($CONFIG->pluginspath . "pages/history.php");
-				break;
-			default:
-				include($CONFIG->pluginspath . "pages/new.php");
-				break;
-		}
 	}
 }
 
